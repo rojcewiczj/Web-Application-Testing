69a893c5bef49101f29b066f8208927f7d0d735f
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-05 17:39:50 -0500

    working on testing

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
 
 
 
@@ -9,3 +11,5 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test("")
